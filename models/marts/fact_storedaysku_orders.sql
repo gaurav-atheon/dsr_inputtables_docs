5cb2ab8e4fc6fa5c0a01108819750060e62958c2
AlexPaterson <alex.paterson@atheon.co.uk>
2021-01-17 20:13:02 +0000

    fixed facts

--- models/marts/fact_storedaysku_orders.sql
+++ models/marts/fact_storedaysku_orders.sql
@@ -1,16 +1,8 @@
-with
-
-Source_db as (
-select organisation_id, business_organisation_number
-from {{ ref('stg_organisation_mapping') }}
-where origin_organisation_id = ( select organisation_id from {{ ref('utl_master_organisation') }} )
-)
-
 select
     day_date,
     src.organisation_id, --converted to DSR ID
-    organisation_location_id, --convert this to DSR ID
-    Organisation_SKU, --convert this to DSR ID
+    loc.LOCATION_ID, --converted to DSR ID
+    prd.Product_ID, --converted to DSR ID
     epos_eaches,
     epos_value,
     rtc_epos_eaches,
@@ -22,5 +14,14 @@ select
     total_waste_value
 
 from {{ ref('stg_storedaysku_orders') }} ord
-inner join Source_db src
+
+left join {{ ref('utl_source_organisations') }} src --this should really be "inner", with relationship validation earlier in the flow
 on ord.source_db_id = src.business_organisation_number
+
+left join {{ ref('dim_location') }} loc --this should really be "inner", with relationship validation earlier in the flow
+on loc.organisation_ID = src.organisation_ID
+and loc.ORGANISATION_LOCATION_ID = ord.ORGANISATION_LOCATION_ID
+
+left join {{ ref('dim_product') }} prd --this should really be "inner", with relationship validation earlier in the flow
+on prd.organisation_ID = src.organisation_ID
+and prd.ORGANISATION_SKU = ord.ORGANISATION_SKU
--- models/marts/utl_organisationsku_visibility.sql
+++ models/marts/utl_organisationsku_visibility.sql
@@ -8,20 +8,20 @@ from {{ ref('int_storedaysku_orders_visibility') }}
 next_order as
 (
 select o.*,
-  lead(o.day_date) over (partition by Organisation_ID, Organisation_SKU, table_reference order by o.day_date asc) next_order_date,
-  datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Organisation_SKU, table_reference order by o.day_date desc)) diff_days_desc,
-  datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Organisation_SKU, table_reference order by o.day_date asc)) diff_days_asc,
+  lead(o.day_date) over (partition by Organisation_ID, Product_ID, table_reference order by o.day_date asc) next_order_date,
+  datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Product_ID, table_reference order by o.day_date desc)) diff_days_desc,
+  datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Product_ID, table_reference order by o.day_date asc)) diff_days_asc,
   case
-    when datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Organisation_SKU, table_reference order by o.day_date desc)) < -1
+    when datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Product_ID, table_reference order by o.day_date desc)) < -1
         then 'Start'
-    when datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Organisation_SKU, table_reference order by o.day_date desc)) is null
+    when datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Product_ID, table_reference order by o.day_date desc)) is null
         then 'Start'
     else 'Continuous'
   end marker_start,
   case
-    when datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Organisation_SKU, table_reference order by o.day_date asc)) > 1
+    when datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Product_ID, table_reference order by o.day_date asc)) > 1
         then 'End'
-    when datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Organisation_SKU, table_reference order by o.day_date asc)) is null
+    when datediff(day, o.day_date, lead(o.day_date) over (partition by Organisation_ID, Product_ID, table_reference order by o.day_date asc)) is null
         then 'End'
     else 'Continuous'
   end marker_end
@@ -30,7 +30,7 @@ select o.*,
 
 end_points as
 (
-select ord.*, lead(ord.day_date) over (partition by Organisation_ID, Organisation_SKU, table_reference order by ord.day_date asc) end_of_run
+select ord.*, lead(ord.day_date) over (partition by Organisation_ID, Product_ID, table_reference order by ord.day_date asc) end_of_run
 from next_order ord
 --Knock out the intervening dates
 where MARKER_START <> 'Continuous'
@@ -43,7 +43,7 @@ select q1.DAY_DATE DAY_DATE_FROM,
   when MARKER_END = 'End' then DAY_DATE
   when END_OF_RUN is null then DAY_DATE
   else END_OF_RUN end DAY_DATE_TO,
-ORGANISATION_ID ORIGIN_ORGANISATION_ID, ORGANISATION_SKU, ORGANISATION_ID SUBJECT_ORGANISATION_ID, table_reference, ACCESS_LEVEL
+ORGANISATION_ID ORIGIN_ORGANISATION_ID, Product_ID, ORGANISATION_ID SUBJECT_ORGANISATION_ID, table_reference, ACCESS_LEVEL
 from end_points q1
 where MARKER_START = 'Start' --Only keep the start of a run
 )

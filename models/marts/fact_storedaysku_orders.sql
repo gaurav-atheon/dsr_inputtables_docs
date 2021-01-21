59c67107a02792d9722f816db72d19e5f4637604
AlexPaterson <alex.paterson@atheon.co.uk>
2021-01-21 12:52:10 +0000

    test changes

--- models/marts/fact_storedaysku_orders.sql
+++ models/marts/fact_storedaysku_orders.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='unique_key'
+    )
+}}
+
 select
     day_date,
     src.organisation_id, --converted to DSR ID
@@ -11,7 +18,9 @@ select
     bin_waste_eaches,
     bin_waste_value,
     total_waste_eaches,
-    total_waste_value
+    total_waste_value,
+    loaded_timestamp,
+    {{ dbt_utils.surrogate_key(['day_date', 'src.organisation_id', 'loc.LOCATION_ID', 'prd.Product_ID']) }} as unique_key
 
 from {{ ref('stg_storedaysku_orders') }} ord
 
@@ -25,3 +34,9 @@ and loc.ORGANISATION_LOCATION_ID = ord.ORGANISATION_LOCATION_ID
 left join {{ ref('dim_product') }} prd --this should really be "inner", with relationship validation earlier in the flow
 on prd.organisation_ID = src.organisation_ID
 and prd.ORGANISATION_SKU = ord.ORGANISATION_SKU
+
+  {% if is_incremental() %}
+
+    where ord.loaded_timestamp >= (select max(loaded_timestamp) from {{this}})
+
+  {% endif %}
--- models/marts/int_storedaysku_orders_visibility.sql
+++ models/marts/int_storedaysku_orders_visibility.sql
@@ -13,3 +13,8 @@ on ord.source_db_id = src.business_organisation_number
 inner join {{ ref('dim_product') }} prd
 on prd.organisation_ID = src.organisation_ID
 and prd.ORGANISATION_SKU = ord.ORGANISATION_SKU
+
+group by
+ord.day_date,
+src.organisation_id,
+prd.Product_ID

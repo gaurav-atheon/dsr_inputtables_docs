abf62f6172a86f300c419cc655264426dadcb811
gaurav-atheon <gaurav.jain@atheon.co.uk>
2021-03-02 12:18:07 +0000

    added code for org calendar

--- models/output/dimensions/calendar/output_dim_org_calendar.sql
+++ models/output/dimensions/calendar/output_dim_org_calendar.sql
@@ -2,5 +2,10 @@ select
     organisation_id,
     day_date,
     week_id,
-    year_id
+    year_id,
+    weekcommencing,
+    weekending,
+    dayofweeknumber,
+    day,
+    loaded_timestamp
 from {{ ref('dim_org_calendar') }}

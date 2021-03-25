070bfdda21fe74e811377043404b1288f02b56e6
gaurav-atheon <gaurav.jain@atheon.co.uk>
2021-03-25 10:44:59 +0000

    correct format

---
+++ models/output/facts/movements/plans/output_fact_pln_mvt_storedaysku.sql
@@ -0,0 +1,18 @@
+select
+    day_date,
+    forecast_date,
+    organisation_id,
+    subject_organisation_id,
+    subject_location_id,
+    product_id,
+    units_required,
+    units_required_upper,
+    units_required_lower,
+    forecast_components,
+    created_at,
+    model_version,
+    origin_file,
+    loaded_timestamp,
+    unique_key
+
+from {{ref('fact_pln_mvt_storedaysku')}}

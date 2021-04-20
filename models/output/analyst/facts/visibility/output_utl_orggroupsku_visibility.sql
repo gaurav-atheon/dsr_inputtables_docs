ffe71eb26e70d6bc272d1d533e090f98dba40fcf
AlexPaterson <alex.paterson@atheon.co.uk>
2021-04-20 19:55:41 +0100

    update output

--- models/output/analyst/facts/visibility/output_utl_orggroupsku_visibility.sql
+++ models/output/analyst/facts/visibility/output_utl_orggroupsku_visibility.sql
@@ -1,6 +1,7 @@
   SELECT table_reference,
          item_id,
-         DAY_DATE,
+         DAY_DATE_FROM,
+         DAY_DATE_TO,
          organisation_group_id,
          min_access_level
   FROM   {{ ref('utl_orggroupsku_visibility') }}

c7131f5e6beeb71f6cdeb2108c1e5b234db19c5c
Vinit Joshi <vinit.joshi@atheon.co.uk>
2021-04-30 09:15:03 +0000

    add ref

--- models/output/application/output_organisation_items.sql
+++ models/output/application/output_organisation_items.sql
@@ -5,7 +5,7 @@ description AS retailerproductdescription,
 to_varchar(prd.attributes:class_name) AS retailerproductcategory,
 to_varchar(prd.attributes:subclass_name) AS retailerproductsubcategory,
 org.organisation_name AS suppliername
-FROM utl_orggroupsku_visibility osv 
-INNER JOIN dim_product prd ON prd.product_id = osv.item_id
-INNER JOIN dim_organisation org ON org.organisation_id = osv.organisation_group_id
-INNER JOIN dim_organisation retailer_org ON retailer_org.organisation_id = prd.ORGANISATION_ID;
+FROM {{ ref('utl_orggroupsku_visibility') }} osv 
+INNER JOIN {{ ref('dim_product') }} prd ON prd.product_id = osv.item_id
+INNER JOIN {{ ref('dim_organisation') }} org ON org.organisation_id = osv.organisation_group_id
+INNER JOIN {{ ref('dim_organisation') }} retailer_org ON retailer_org.organisation_id = prd.ORGANISATION_ID

8ddc8f190d3be61682c741c7d1b65e91da705f86
gaurav-atheon <gaurav.jain@atheon.co.uk>
2021-04-30 16:08:26 +0100

    added location_function to visibility

--- models/facts/inventory/actuals/fact_act_inv_locationdaysku.sql
+++ models/facts/inventory/actuals/fact_act_inv_locationdaysku.sql
@@ -61,11 +61,11 @@ and loc.location_function = inv.location_function
 inner join {{ ref('dim_logisticitem') }} prd --need relationship validation earlier in the flow
 on prd.organisation_id = src.organisation_id
 and prd.organisation_case = inv.organisation_case
-
-        {% if is_incremental() %}
-        where inv.loaded_timestamp > nvl((select max(loaded_timestamp) from {{ this }} where source = 'case'), to_timestamp('0'))
-        {% endif %}
 where prd.product_id is not null
+        {% if is_incremental() %}
+        and inv.loaded_timestamp > nvl((select max(loaded_timestamp) from {{ this }} where source = 'case'), to_timestamp('0'))
+        {% endif %}
+
 group by
     day_date,
     src.organisation_id, --converted to dsr id

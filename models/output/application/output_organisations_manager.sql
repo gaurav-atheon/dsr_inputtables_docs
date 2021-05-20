7919d86f33932154962962864d7d312434ee0868
Vinit Joshi <vinit.joshi@atheon.co.uk>
2021-05-20 13:40:21 +0000

    refactor organisation view

--- models/output/application/output_organisations_manager.sql
+++ models/output/application/output_organisations_manager.sql
@@ -1,3 +1,10 @@
-select distinct do.organisation_name, business_organisation_number, dom.origin_organisation_number
-from {{ ref('dim_organisation') }} do
-inner join {{ref('dim_organisation_mapping')}} dom on do.organisation_id = dom.organisation_id
+SELECT DISTINCT dom.origin_organisation_number, 
+                dom.business_organisation_number, 
+                business_organisation_name 
+FROM   {{ ref('dim_organisation_mapping') }} dom 
+       LEFT JOIN {{ ref('utl_master_organisation') }} umo 
+              ON dom.origin_organisation_id = umo.organisation_id 
+       INNER JOIN {{ ref('dim_organisation_parentage') }} dop 
+               ON dop.parent_organisation_id = dom.organisation_id 
+       INNER JOIN {{ ref('utl_organisation_parents') }} uop 
+               ON uop.parent_organisation_id = dop.parent_organisation_id;

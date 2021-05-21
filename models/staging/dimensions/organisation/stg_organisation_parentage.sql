5fc59204b25eb2fba0e660d900bc7a15ef319aed
gaurav-atheon <gaurav.jain@atheon.co.uk>
2021-05-21 16:41:06 +0100

    Fixed issue with multiple entries in location when organisation_location_id is null or blank and the issue when there is an orphan organisation id in organisation parentage table

--- models/staging/dimensions/organisation/stg_organisation_parentage.sql
+++ models/staging/dimensions/organisation/stg_organisation_parentage.sql
@@ -9,24 +9,27 @@ with
 ranked_data as
 (
 select
-    origin_organisation_number,
-    business_organisation_number,
-    parent_origin_organisation_number,
-    parent_business_organisation_number,
-    creator_origin_organisation_number,
-    creator_business_organisation_number,
-    loaded_timestamp,
-    created_timestamp,
-    {{ dbt_utils.surrogate_key(['origin_organisation_number','business_organisation_number',
-                                'creator_origin_organisation_number','creator_business_organisation_number']) }} as parentage_id,
-    row_number() over (partition by origin_organisation_number,business_organisation_number,
-                                    creator_origin_organisation_number,creator_business_organisation_number order by loaded_timestamp desc) rank
+    op.origin_organisation_number,
+    op.business_organisation_number,
+    op.parent_origin_organisation_number,
+    op.parent_business_organisation_number,
+    op.creator_origin_organisation_number,
+    op.creator_business_organisation_number,
+    op.loaded_timestamp,
+    op.created_timestamp,
+    {{ dbt_utils.surrogate_key(['op.origin_organisation_number','op.business_organisation_number',
+                                'op.creator_origin_organisation_number','op.creator_business_organisation_number']) }} as parentage_id,
+    row_number() over (partition by op.origin_organisation_number,op.business_organisation_number,
+                                    op.creator_origin_organisation_number,op.creator_business_organisation_number order by op.loaded_timestamp desc) rank
  {% if target.name == 'ci' %}
-    from {{ ref ('stg_organisation_parentage_ci' )}}
+    from {{ ref ('stg_organisation_parentage_ci' )}} op
  {% else %}
-    from {{ source('dsr_input', 'input_organisation_parentage') }}
+    from {{ source('dsr_input', 'input_organisation_parentage') }} op
+    inner join {{ ref ('stg_organisation_mapping' )}} om
+    on op.parent_business_organisation_number = om.business_organisation_number
+    and op.parent_origin_organisation_number = om.origin_organisation_number
         {% if is_incremental() %}
-        where loaded_timestamp > nvl((select max(loaded_timestamp) from {{ this }}), to_timestamp('0'))
+        where op.loaded_timestamp > nvl((select max(loaded_timestamp) from {{ this }}), to_timestamp('0'))
         {% endif %}
  {% endif %}
 )

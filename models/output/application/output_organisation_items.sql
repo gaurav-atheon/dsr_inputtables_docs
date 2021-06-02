7a7629ce1c4b225fb9153958b95df14451b491f5
Vinit Joshi <vinit.joshi@atheon.co.uk>
2021-06-02 15:56:48 +0000

    remove products with no hostname

--- models/output/application/output_organisation_items.sql
+++ models/output/application/output_organisation_items.sql
@@ -10,3 +10,4 @@ INNER JOIN {{ ref('dim_product') }} prd ON prd.product_id = osv.item_id
 INNER JOIN {{ ref('dim_organisation') }} org ON org.organisation_id = osv.organisation_group_id
 INNER JOIN {{ ref('dim_organisation') }} retailer_org ON retailer_org.organisation_id = prd.ORGANISATION_ID
 INNER JOIN {{ ref('dim_organisation_mapping') }} dom on dom.organisation_id = org.organisation_id
+where origin_organisation_number = '1';

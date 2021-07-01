90b94ef51659e6734739540a183c7df175147ce4
Vinit Joshi <vinit.joshi@atheon.co.uk>
2021-07-01 12:10:49 +0000

    amended view

--- models/output/application/output_organisations_manager.sql
+++ models/output/application/output_organisations_manager.sql
@@ -1,10 +1,35 @@
-SELECT DISTINCT dom.origin_organisation_number, 
-                dom.business_organisation_number, 
-                business_organisation_name 
-FROM   {{ ref('dim_organisation_mapping') }} dom 
-       LEFT JOIN {{ ref('utl_master_organisation') }} umo 
-              ON dom.origin_organisation_id = umo.organisation_id 
-       INNER JOIN {{ ref('dim_organisation_parentage') }} dop 
-               ON dop.parent_organisation_id = dom.organisation_id 
-       INNER JOIN {{ ref('utl_organisation_parents') }} uop 
-               ON uop.parent_organisation_id = dop.parent_organisation_id
+WITH retailer_source -- get all retailer organisation_ids  
+     AS (SELECT organisation_id, organisation_name 
+         FROM   {{ ref('dim_organisation') }} 
+         WHERE  organisation_type = 'retailer'), 
+     retailer_organisations 
+     -- display organisations where the origin of the organisation is the retailer  
+     AS (SELECT r.organisation_id AS retailer_id,
+                r.organisation_name,
+                om.organisation_id, 
+                om.business_organisation_name, 
+                om.business_organisation_number 
+         FROM   {{ ref('dim_organisation_mapping') }} om 
+                INNER JOIN retailer_source r 
+                        ON om.origin_organisation_id = r.organisation_id), 
+     organisation_parentage 
+     -- find the parent organisation_id for all retailer organisations and display that  
+     AS (SELECT ro.*, 
+                op.parent_organisation_id 
+         FROM   retailer_organisations ro 
+                LEFT JOIN {{ ref('dim_organisation_parentage') }} op 
+                       -- check to see if organisation has a parent 
+                       ON op.subject_organisation_id = ro.organisation_id), 
+     check_parent 
+     AS (SELECT op.*, 
+                om.origin_organisation_id       AS origin_of_parent_id, 
+                origin_organisation_number      AS origin_of_parent_number, 
+                om.business_organisation_number AS host_name 
+         FROM   organisation_parentage op 
+                LEFT JOIN {{ ref('dim_organisation_mapping') }} om 
+                       ON op.parent_organisation_id = om.organisation_id) 
+SELECT organisation_name as retailer, business_organisation_number, business_organisation_name
+FROM   check_parent 
+WHERE  origin_of_parent_number = '1' 
+        OR origin_of_parent_number IS NULL 
+-- if origin of parent is null or Atheon then display
